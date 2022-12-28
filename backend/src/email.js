2b629081005918655f5f80c31ccdf6489db3c9bf
Ruchit Patel <ruchitnpatel@gmail.com>
2022-12-28 12:32:24 -0800

    Update email.js

--- backend/src/email.js
+++ backend/src/email.js
@@ -1,19 +1,65 @@
 const db = require('./db');
 
+// https://gist.github.com/johnelliott/cf77003f72f889abbc3f32785fa3df8d
+const temp =
+['^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-',
+  '[89AB][0-9A-F]{3}-[0-9A-F]{12}$'].join('');
+const uuidReg = new RegExp(temp, 'i');
+
 exports.getAll = async (req, res) => {
-    console.log("GET ALL");
-    console.log("geakjhgkdjhf")
-    res.status(200).send();
+  const from = req.query.from;
+  let re;
+  const mbx = req.query.mailbox;
+  if (from !== undefined) re = await db.fromMail(mbx, from);
+  else re = await db.selectMails(mbx);
+  if (re.length === 0) {
+    res.status(404).send();
+  } else {
+    res.status(200).send(re);
+  }
 };
 
 exports.getById = async (req, res) => {
-    console.log("GET BY ID");
+  const id = req.params.id;
+  if (!id.match(uuidReg)) {
+    res.status(404).send();
+    return;
+  }
+  const mail = await db.selectMailId(id);
+  if (mail === undefined) {
+    res.status(404).send();
+  } else {
+    res.status(200).json(mail);
+  }
 };
 
 exports.post = async (req, res) => {
-    console.log("POST BY ID");
+  const date = new Date().toISOString();
+  mail = {
+    'to': {
+      'name': req.body.to.name,
+      'email': req.body.to.email,
+    },
+    'from': {
+      'name': 'CSE186 Student',
+      'email': 'CSE186student@ucsc.edu',
+    },
+    'received': date,
+    'sent': date,
+    'content': req.body.content,
+    'subject': req.body.subject,
+  };
+  const val = await db.insertMail(mail);
+  res.status(200).send(val);
 };
 
 exports.put = async (req, res) => {
-    console.log("PUT");
+  const id = req.params.id;
+  const mbx = req.query.mailbox;
+  if (!id.match(uuidReg)) {
+    res.status(404).send();
+    return;
+  }
+  const val = await db.putMail(id, mbx);
+  res.status(val).send();
 };
