2c70cc982037e80f5c2d9b3e588ba0f8de31bf61
Ruchit Patel <ruchitnpatel@gmail.com>
2022-12-28 12:29:37 -0800

    Update schema.sql

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -2,3 +2,7 @@
 DROP TABLE IF EXISTS people;
 
 CREATE TABLE people(email varchar(30) PRIMARY KEY, pname VARCHAR(20), hash VARCHAR(256));
+
+DROP TABLE IF EXISTS mail;
+
+CREATE TABLE mail(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), mailbox VARCHAR(32), mail jsonb);
